9184e156d39222ea02930cd4d29a70f775c437a8
vinsekani <vinsekani@gmail.com>
2025-02-04 10:53:11 +0300

    changed port number from 3000 to 8000

--- src/server.js
+++ src/server.js
@@ -1,32 +1,31 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth");
-dotenv.config()
-const app =express()
-const PORT = process.env.PORT || 3000;
+dotenv.config();
+const app = express();
+const PORT = 8000 || process.env.PORT;
 
-app.get("/", (req, res) =>{
-    res.json({ message: "HabitaPay, Streamline your contacts with sekani only" });
-})
-
+app.get("/", (req, res) => {
+  res.json({ message: "HabitaPay, Streamline your contacts with sekani only" });
+});
 
 // READING JSON
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // API ROUTES
 app.use("/api/auth", authRoutes);
 
 mongoose
-.connect(process.env.MONGO_URI)
-.then(() => {
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     app.listen(PORT, () => {
       console.log(
         `Server is running on http://localhost:${PORT} and DB is connected`
       );
     });
   })
-  .catch((err) => console.log(err))
+  .catch((err) => console.log(err));
